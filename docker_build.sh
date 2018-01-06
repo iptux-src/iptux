b9446964afcdc9ee0de0fe0d79bd33f90a20bebb
LI Daobing <lidaobing@gmail.com>
2018-01-06 21:51:42 +0800

    add -y

--- docker_build.sh
+++ docker_build.sh
@@ -4,6 +4,6 @@ set -ex
 
 cd `dirname $0`
 apt-get update
-apt-get install libgtk2.0-dev libglib2.0-dev libgconf2-dev libgstreamer1.0-dev g++ make cmake
+apt-get install -y libgtk2.0-dev libglib2.0-dev libgconf2-dev libgstreamer1.0-dev g++ make cmake
 mkdir build && cd build && cmake .. && make
 make install
