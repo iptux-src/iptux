ec756873c8aad673ff239ef478b3050f3b43e189
LI Daobing <lidaobing@gmail.com>
2018-02-04 00:20:30 +0800

    use customized docker

--- scripts/build_in_docker_ubuntu1604.sh
+++ scripts/build_in_docker_ubuntu1604.sh
@@ -3,7 +3,5 @@
 set -ex
 
 cd `dirname $0`/..
-apt-get update
-apt-get install -y libgtk-3-dev libglib2.0-dev libgstreamer1.0-dev libjsoncpp-dev g++ make cmake
 mkdir build && cd build && cmake .. && make && ctest --verbose
 make install
