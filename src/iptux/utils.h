f4f471211e89a963fb5f5fb1c1c618d654a6d23d
LI Daobing <lidaobing@gmail.com>
2018-02-10 01:53:42 +0800

    remove unused code

--- src/iptux/utils.h
+++ src/iptux/utils.h
@@ -42,8 +42,6 @@ void ipv4_order(in_addr_t *ip1, in_addr_t *ip2);
 
 char *iptux_string_validate(const char *string, const std::string &codeset,
                             char **encode);
-char *iptux_string_validate_copy(const char *string, const char *codeset,
-                                 char **encode);
 char *convert_encode(const char *string, const char *tocode,
                      const char *fromcode);
 char *convert_encode_copy(const char *string, const char *tocode,
@@ -51,7 +49,6 @@ char *convert_encode_copy(const char *string, const char *tocode,
 
 void get_file_system_info(const char *path, int64_t *avail, int64_t *total);
 
-char *iptux_string_getline(const char *str);
 char *assert_filename_inexist(const char *path);
 char *getformattime(gboolean date, const char *format, ...);
 
