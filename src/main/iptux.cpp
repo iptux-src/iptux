698aec3494782d5afba521fb10ca460be12b561d
LI Daobing <lidaobing@gmail.com>
2018-02-09 20:00:30 +0800

    print backtrace when sigsegv or sigabrt

--- src/main/iptux.cpp
+++ src/main/iptux.cpp
@@ -21,6 +21,7 @@
 
 #include <glib.h>
 #include <libintl.h>
+#include <execinfo.h>
 
 #include "iptux/Application.h"
 #include "iptux/SoundSystem.h"
@@ -120,7 +121,26 @@ static void dealLog(const IptuxConfig& config) {
   }
 }
 
+
+
+static void segvHandler(int sig) {
+  void *array[99];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 99);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
+
 int main(int argc, char** argv) {
+  signal(SIGSEGV, segvHandler);
+  signal(SIGABRT, segvHandler);
+  signal(SIGTRAP, segvHandler);
   setlocale(LC_ALL, "");
   bindtextdomain(GETTEXT_PACKAGE, __LOCALE_PATH);
   bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
