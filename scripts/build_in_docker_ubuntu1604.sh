6ed07cd03545bd2ea8c7fdb4ea7473f95d899750
LI Daobing <lidaobing@gmail.com>
2018-02-21 01:13:07 +0800

    remove test

--- scripts/build_in_docker_ubuntu1604.sh
+++ scripts/build_in_docker_ubuntu1604.sh
@@ -12,6 +12,7 @@ make install
 lcov --directory . --capture --output-file coverage.info # capture coverage info
 lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
 lcov --remove coverage.info '*Test*' --output-file coverage.info # filter out system
+lcov --remove coverage.info '*gtest*' --output-file coverage.info # filter out system
 lcov --list coverage.info #debug info
 # Uploading report to CodeCov
 bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
