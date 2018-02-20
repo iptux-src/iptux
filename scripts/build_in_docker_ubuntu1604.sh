17e9aee082b1fa681142c3bffbb67bef64c57760
LI Daobing <lidaobing@gmail.com>
2018-02-21 01:03:35 +0800

    fix script

--- scripts/build_in_docker_ubuntu1604.sh
+++ scripts/build_in_docker_ubuntu1604.sh
@@ -2,6 +2,16 @@
 
 set -ex
 
+sudo apt-get install -y lcov
+
 cd `dirname $0`/..
-mkdir build && cd build && cmake .. && make && ctest --verbose
+mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE .. && make
+make iptux_coverage
+ctest --verbose
 make install
+lcov --directory . --capture --output-file coverage.info # capture coverage info
+lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
+lcov --remove coverage.info '*Test*' --output-file coverage.info # filter out system
+lcov --list coverage.info #debug info
+# Uploading report to CodeCov
+bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
