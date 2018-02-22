0eac4969a4c651980e02c7c130db149e26954655
LI Daobing <lidaobing@gmail.com>
2018-02-22 19:45:10 +0800

    fix critical warning

--- src/iptux/Application.cpp
+++ src/iptux/Application.cpp
@@ -23,16 +23,13 @@ namespace iptux {
 
 Application::Application(IptuxConfig& config)
 : config(config),
+  data(nullptr),
+  window(nullptr),
   shareFile(nullptr) {
-  data = new ProgramData(config);
   app = gtk_application_new ("io.github.iptux-src.iptux", G_APPLICATION_FLAGS_NONE);
   g_signal_connect_swapped(app, "startup", G_CALLBACK(onStartup), this);
   g_signal_connect_swapped(app, "activate", G_CALLBACK(onActivate), this);
   g_signal_connect_swapped(app, "notify::active-window", G_CALLBACK(onActiveWindowChanged), this);
-  window = new MainWindow(app, config, *data);
-
-  g_progdt = data;
-  g_mwin = window;
 }
 
 Application::~Application() {
@@ -46,6 +43,10 @@ int Application::run(int argc, char** argv) {
 }
 
 void Application::onStartup(Application& self) {
+  self.data = new ProgramData(self.config);
+  self.window = new MainWindow(self.app, self.config, *self.data);
+  g_progdt = self.data;
+  g_mwin = self.window;
   iptux_register_resource();
   GActionEntry app_entries[] =  {
       { "quit", G_ACTION_CALLBACK(onQuit), NULL, NULL, NULL },
@@ -73,8 +74,6 @@ void Application::onActivate(Application& self) {
   g_lgsys = new LogSystem(*self.data);
 
   self.window->SetStatusIcon(sicon);
-
-  int port = self.config.GetInt("port", IPTUX_DEFAULT_PORT);
   self.window->CreateWindow();
   try {
     g_cthrd->start();
