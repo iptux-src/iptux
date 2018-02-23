21aec3c07f3ecf555f0c801865c6206e2f1f6ba1
LI Daobing <lidaobing@gmail.com>
2018-02-24 01:49:04 +0800

    WriteProgData

--- src/iptux/ProgramDataCoreTest.cpp
+++ src/iptux/ProgramDataCoreTest.cpp
@@ -9,6 +9,7 @@ TEST(ProgramDataCore, Constructor) {
   auto config = newTestIptuxConfig();
   ProgramDataCore* core = new ProgramDataCore(*config);
   ASSERT_FALSE(core->IsSaveChatHistory());
+  core->WriteProgData();
   delete core;
 }
 
