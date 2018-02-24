adbd3e43f3365f4ef7e6774a41d1acc1e5fdeb87
LI Daobing <lidaobing@gmail.com>
2018-02-25 00:07:14 +0800

    don't use g_uuid_string_random

--- src/iptux/TestHelper.cpp
+++ src/iptux/TestHelper.cpp
@@ -1,16 +1,16 @@
 #include "config.h"
 #include "TestHelper.h"
 
+#include <glib.h>
+
 using namespace std;
 
 namespace iptux {
 
 shared_ptr<IptuxConfig> newTestIptuxConfig() {
-  char* uuid = g_uuid_string_random();
-  char* fname = g_strdup_printf("/tmp/iptux%s.json", uuid);
+  char* fname = g_strdup_printf("/tmp/iptux%d.json", g_random_int());
   auto res = shared_ptr<IptuxConfig>(new IptuxConfig(fname));
   g_free(fname);
-  g_free(uuid);
   return res;
 }
 
