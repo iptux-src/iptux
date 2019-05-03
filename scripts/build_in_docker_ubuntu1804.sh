06f5ba1b6b1cd481158df26f64359d2e2d24bc94
LI Daobing <lidaobing@gmail.com>
2019-05-03 21:14:15 +0800

    Update build_in_docker_ubuntu1804.sh

--- scripts/build_in_docker_ubuntu1804.sh
+++ scripts/build_in_docker_ubuntu1804.sh
@@ -7,14 +7,13 @@ export CODECOV_TOKEN=cec2d3eb-e3d2-414c-ae88-137cc880e0e1
 env
 
 cd $(dirname $0)/..
-meson builddir && ninja -v -C builddir test
-xvfb-run make iptux_coverage VERBOSE=1
-xvfb-run ctest --verbose
+meson builddir && ninja -v -C builddir
+xvfb-run ninja -v -C builddir test
 make install
-lcov --directory . --capture --output-file coverage.info # capture coverage info
-lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
-lcov --remove coverage.info '*Test*' --output-file coverage.info # filter out system
-lcov --remove coverage.info '*gtest*' --output-file coverage.info # filter out system
-lcov --list coverage.info #debug info
-# Uploading report to CodeCov
-bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
+# lcov --directory . --capture --output-file coverage.info # capture coverage info
+# lcov --remove coverage.info '/usr/*' --output-file coverage.info # filter out system
+# lcov --remove coverage.info '*Test*' --output-file coverage.info # filter out system
+# lcov --remove coverage.info '*gtest*' --output-file coverage.info # filter out system
+# lcov --list coverage.info #debug info
+# # Uploading report to CodeCov
+# bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
