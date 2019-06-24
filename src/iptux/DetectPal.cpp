4088e54d1bf3ae26bac5cf1efcdfa64ee3d27898
LI Daobing <lidaobing@gmail.com>
2019-06-25 00:44:15 +0800

    refactor detect pal

--- src/iptux/DetectPal.cpp
+++ src/iptux/DetectPal.cpp
@@ -12,115 +12,54 @@
 #include "config.h"
 #include "DetectPal.h"
 
-#include "iptux/UiCoreThread.h"
-#include "iptux/callback.h"
-#include "iptux-core/deplib.h"
+#include <glib/gi18n.h>
+#include <glog/logging.h>
+
 #include "iptux/global.h"
-#include "iptux-core/output.h"
 #include "iptux/UiHelper.h"
 
 namespace iptux {
 
 /**
- * 类构造函数.
- */
-DetectPal::DetectPal() : widset(NULL) { g_datalist_init(&widset); }
-
-/**
- * 类析构函数.
- */
-DetectPal::~DetectPal() { g_datalist_clear(&widset); }
-
-/**
- * 探测好友入口.
- * @param parent 父窗口指针
- */
-void DetectPal::DetectEntry(GtkWidget *parent) {
-  GtkWidget *dialog;
-  DetectPal dpal;
-
-  /* 创建对话框窗体 */
-  dialog = dpal.CreateMainDialog(parent);
-  gtk_box_pack_start(GTK_BOX(gtk_dialog_get_content_area(GTK_DIALOG(dialog))),
-                     dpal.CreateInputArea(), TRUE, TRUE, 0);
-
-  /* 运行对话框 */
-  gtk_widget_show_all(dialog);
-mark:
-  switch (gtk_dialog_run(GTK_DIALOG(dialog))) {
-    case GTK_RESPONSE_ACCEPT:
-      dpal.SendDetectPacket();
-      goto mark;
-    default:
-      break;
-  }
-  gtk_widget_destroy(dialog);
-}
-
-/**
- * 创建主对话框.
- * @param parent 父窗口指针
- * @return 对话框
- */
-GtkWidget *DetectPal::CreateMainDialog(GtkWidget *parent) {
-  GtkWidget *dialog;
-
-  dialog = gtk_dialog_new_with_buttons(
-      _("Detect pals"), GTK_WINDOW(parent), GtkDialogFlags(GTK_DIALOG_MODAL),
-      _("Detect"), GTK_RESPONSE_ACCEPT, _("Cancel"), GTK_RESPONSE_CANCEL, NULL);
-  gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_ACCEPT);
-  gtk_window_set_resizable(GTK_WINDOW(dialog), FALSE);
-  gtk_container_set_border_width(GTK_CONTAINER(dialog), 5);
-  g_datalist_set_data(&widset, "dialog-widget", dialog);
-
-  return dialog;
-}
-
-/**
- * 创建接受输入区域.
- * @return 主窗体
- */
-GtkWidget *DetectPal::CreateInputArea() {
-  GtkWidget *frame;
-  GtkWidget *widget;
-
-  frame = gtk_frame_new(_("Please input an IP address (IPv4 only):"));
-  gtk_frame_set_shadow_type(GTK_FRAME(frame), GTK_SHADOW_ETCHED_IN);
-
-  widget = gtk_entry_new();
-  gtk_entry_set_max_length(GTK_ENTRY(widget), INET_ADDRSTRLEN);
-  g_object_set(widget, "has-tooltip", TRUE, NULL);
-  gtk_container_add(GTK_CONTAINER(frame), widget);
-  g_signal_connect(widget, "query-tooltip", G_CALLBACK(entry_query_tooltip),
-                   _("Please input an IP address (IPv4 only)!"));
-  g_signal_connect(widget, "insert-text", G_CALLBACK(entry_insert_numeric),
-                   NULL);
-  g_datalist_set_data(&widset, "ipv4-entry-widget", widget);
-
-  return frame;
-}
-
-/**
  * 发送探测数据包.
  */
-void DetectPal::SendDetectPacket() {
-  GtkWidget *widget, *parent;
+static bool sendDetectPacket(const char* ipv4Text) {
   in_addr ipv4;
-  const char *text;
-
-  parent = GTK_WIDGET(g_datalist_get_data(&widset, "dialog-widget"));
-  widget = GTK_WIDGET(g_datalist_get_data(&widset, "ipv4-entry-widget"));
-  gtk_widget_grab_focus(widget);  //为下一次任务做准备
-
-  text = gtk_entry_get_text(GTK_ENTRY(widget));
-  if (inet_pton(AF_INET, text, &ipv4) <= 0) {
-    pop_warning(parent, _("\nIllegal IP(v4) address: %s!"), text);
-    return;
+  if (inet_pton(AF_INET, ipv4Text, &ipv4) <= 0) {
+    return false;
   }
-
   g_cthrd->SendDetectPacket(ipv4);
-  pop_info(parent, _("The notification has been sent to %s."), text);
-  gtk_entry_set_text(GTK_ENTRY(widget), "");
+  return true;
+}
+
+DetectPal::DetectPal(GtkBuilder* builder, GtkWindow* window) {
+  this->detectPalDialog = CHECK_NOTNULL(GTK_DIALOG(
+      gtk_builder_get_object(builder, "detect_pal_dialog")));
+  gtk_window_set_transient_for(GTK_WINDOW(this->detectPalDialog), window);
+  this->detectPalIpv4Entry = CHECK_NOTNULL(GTK_ENTRY(
+      gtk_builder_get_object(builder, "detect_pal_ipv4_entry")));
+}
+
+void DetectPal::run() {
+  bool loop = true;
+  while(loop) {
+    const char* ipv4Text = nullptr;
+    switch(gtk_dialog_run(detectPalDialog)) {
+      case GTK_RESPONSE_ACCEPT:
+        ipv4Text = gtk_entry_get_text(detectPalIpv4Entry);
+        if(sendDetectPacket(ipv4Text)) {
+          pop_info(GTK_WIDGET(detectPalDialog), _("The notification has been sent to %s."), ipv4Text);
+          gtk_entry_set_text(GTK_ENTRY(detectPalIpv4Entry), "");
+        } else {
+          pop_warning(GTK_WIDGET(detectPalDialog), _("\nIllegal IP(v4) address: %s!"), ipv4Text);
+        }
+        break;
+      default:
+        loop = false;
+        break;
+    }
+  }
+  gtk_widget_hide(GTK_WIDGET(detectPalDialog));
 }
 
 }  // namespace iptux
--- src/iptux/UiCallbacks.h
+++ src/iptux/UiCallbacks.h
@@ -2,10 +2,17 @@
 #define IPTUX_UI_CALLBACKS_H
 
 #include <gtk/gtk.h>
+#include <gdk/gdkkeysyms.h>
 
 extern "C" {
   G_MODULE_EXPORT
   gboolean iptux_on_activate_link(GtkAboutDialog *label, gchar *uri, gpointer user_data);
+
+  G_MODULE_EXPORT
+  void iptux_on_ipv4_entry_insert_text(GtkWidget *entry, gchar *text, gint length);
+
+  G_MODULE_EXPORT
+  gboolean iptux_on_detect_pal_ipv4_entry_escape(GtkWidget *widget, GdkEventKey *event, gpointer user_data);
 }
 
 #endif
