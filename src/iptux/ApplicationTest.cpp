636bc7c9e7a2288b0c6b20d505cd88ee1b628644
LI Daobing <lidaobing@gmail.com>
2021-03-07 22:09:23 +0800

    add app test

---
+++ src/iptux/ApplicationTest.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+
+#include "iptux/Application.h"
+#include "iptux-core/TestHelper.h"
+
+using namespace std;
+using namespace iptux;
+
+TEST(Application, Constructor) {
+  gtk_init(nullptr, nullptr);
+  auto config = newTestIptuxConfig();
+  Application app(config);
+}
