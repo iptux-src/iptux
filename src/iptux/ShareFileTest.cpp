9562dca840352692d0cb6dc130831f100c2c0aa6
LI Daobing <lidaobing@gmail.com>
2021-03-12 22:39:15 +0800

    add test for sharefile

---
+++ src/iptux/ShareFileTest.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+
+#include "iptux/ShareFile.h"
+#include "iptux-core/TestHelper.h"
+#include "iptux/Application.h"
+#include "iptux/MainWindow.h"
+
+using namespace std;
+using namespace iptux;
+
+TEST(ShareFile, Constructor) {
+  gtk_init(nullptr, nullptr);
+  auto config = newTestIptuxConfig();
+  Application app(config);
+  app.startup();
+  app.activate();
+
+  ShareFile* shareFile = share_file_new(GTK_WINDOW(app.getMainWindow()->getWindow()));
+  gtk_widget_destroy(GTK_WIDGET(shareFile));
+}
