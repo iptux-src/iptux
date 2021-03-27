016cd72ec93f898da4ba869c0966c46e65ce375c
LI Daobing <lidaobing@gmail.com>
2021-03-27 22:59:36 +0800

    Update UiHelperTest.cpp

--- src/iptux/UiHelperTest.cpp
+++ src/iptux/UiHelperTest.cpp
@@ -14,3 +14,10 @@ TEST(UiHelper, utf8MakeValid) {
   ASSERT_EQ(utf8MakeValid("\xe4\xe6\x96\x87"), "�文");
   ASSERT_EQ(utf8MakeValid("\xe4\xb8\xad\xe6"), "中�");
 }
+
+TEST(UiHelper, markupEscapeText) {
+  ASSERT_EQ(markupEscapeText(""), "");
+  ASSERT_EQ(markupEscapeText("中文"), "中文");
+  ASSERT_EQ(markupEscapeText("<span>"), "&lt;span&gt;");
+  ASSERT_EQ(markupEscapeText("\"hello\""), "&quot;hello&quot;");
+}
