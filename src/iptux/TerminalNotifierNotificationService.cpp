9dba670adeb8f689d7dd200d96749d260c3fbf3a
LI Daobing <lidaobing@gmail.com>
2021-04-03 12:24:50 +0800

    Update TerminalNotifierNotificationService.cpp

--- src/iptux/TerminalNotifierNotificationService.cpp
+++ src/iptux/TerminalNotifierNotificationService.cpp
@@ -1,8 +1,9 @@
 #include "config.h"
 #include "iptux/TerminalNotifierNotificationService.h"
+#include "iptux-utils/output.h"
 #include "iptux-utils/utils.h"
 
-#include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -13,14 +14,37 @@ void TerminalNotifierNoticationService::sendNotification(
     const std::string& id,
     const std::string& title,
     const std::string& body,
-    GNotificationPriority priority,
-    GIcon* icon) {
-  cout << stringFormat(
-              "terminal-notifier -group \"%s\" -title \"%s\" -subtitle \"%s\" "
-              "-message \"%s\"",
-              id.c_str(), title.c_str(), g_application_get_application_id(app),
-              body.c_str())
-       << endl;
+    GNotificationPriority,
+    GIcon*) {
+  vector<string> args = {"terminal-notifier",
+                         "-group",
+                         id,
+                         "-title",
+                         title,
+                         "-subtitle",
+                         g_application_get_application_id(app),
+                         "-message",
+                         body};
+  char** args2 = new char*[args.size() + 1];
+  for (int i = 0; i < int(args.size()); ++i) {
+    args2[i] = g_strdup(args[i].c_str());
+  }
+  args2[args.size()] = nullptr;
+  GError* error = nullptr;
+  gint exit_status = 0;
+  gboolean res = g_spawn_sync(
+      nullptr, args2, nullptr,
+      (GSpawnFlags)(G_SPAWN_SEARCH_PATH | G_SPAWN_FILE_AND_ARGV_ZERO |
+                    G_SPAWN_STDOUT_TO_DEV_NULL | G_SPAWN_STDERR_TO_DEV_NULL),
+      nullptr, nullptr, nullptr, nullptr, &exit_status, &error);
+  if (!res) {
+    LOG_WARN("g_spawn_sync failed: %s-%d-%s", g_quark_to_string(error->domain),
+             error->code, error->message);
+  }
+  if (!g_spawn_check_exit_status(exit_status, &error)) {
+    LOG_WARN("g_spawn_sync failed: %s-%d-%s", g_quark_to_string(error->domain),
+             error->code, error->message);
+  }
 }
 
 }  // namespace iptux
