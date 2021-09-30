bc9f3be4fc9848ddfbee5ddf6daaed4d4c8ea124
LI Daobing <lidaobing@gmail.com>
2021-09-30 15:17:36 +0800

    Update MainWindow.cpp

--- src/gtkmm4/MainWindow.cpp
+++ src/gtkmm4/MainWindow.cpp
@@ -3,8 +3,17 @@
 namespace iptux {
 
 MainWindow::MainWindow() {
-  set_title("Basic application");
+  set_title("Iptux");
   set_default_size(200, 200);
+
+  auto hbox = Gtk::manage(new Gtk::Box());
+  set_child(*hbox);
+
+  auto vbox = Gtk::manage(new Gtk::Box(Gtk::Orientation::VERTICAL));
+  hbox->append(*vbox);
+
+  auto label = Gtk::manage(new Gtk::Label("some buttons in 64x64px"));
+  vbox->append(*label);
 }
 
-}
+}  // namespace iptux
