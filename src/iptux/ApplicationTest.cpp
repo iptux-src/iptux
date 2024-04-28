f5303449b9a2533d6e70ef5939ce7305a9913385
LI Daobing <lidaobing@gmail.com>
2024-04-28 12:15:10 -0700

    add test for app

--- src/iptux/ApplicationTest.cpp
+++ src/iptux/ApplicationTest.cpp
@@ -6,7 +6,14 @@
 using namespace std;
 using namespace iptux;
 
+void do_action(Application* app, const string& name) {
+  GActionMap* m = G_ACTION_MAP(app->getApp());
+  g_action_activate(g_action_map_lookup_action(m, name.c_str()), NULL);
+}
+
+
 TEST(Application, Constructor) {
   Application* app = CreateApplication();
+  do_action(app, "help.whats_new");
   DestroyApplication(app);
 }
