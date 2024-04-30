17c9d0fd9961b77adabf6ca998770822ea780ec0
LI Daobing <lidaobing@gmail.com>
2024-04-29 21:19:34 -0700

    fix sh

--- scripts/update-po.sh
+++ scripts/update-po.sh
@@ -8,4 +8,7 @@ git ls-files '*.cpp' '*.ui' '*.metainfo.xml' \
   > po/POTFILES
 ninja -C "${MESON_BUILD_ROOT}" iptux-pot
 ninja -C "${MESON_BUILD_ROOT}" iptux-update-po
-for f in po/*.po; do echo -n "$f: "; msgfmt -v $f; done
+for f in po/*.po; do
+  echo -n "$f: "
+  msgfmt -v "$f"
+done
