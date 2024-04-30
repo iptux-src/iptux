ac51cbe942670fba5d73a42a46f7dc22a71720ca
LI Daobing <lidaobing@gmail.com>
2024-04-29 21:12:14 -0700

    update po

---
+++ scripts/update-po.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+cd "${MESON_SOURCE_ROOT}" || exit 1
+git ls-files *.cpp *.desktop.in *.ui *.metainfo.xml \
+  | grep -v Test \
+  | LC_ALL=C sort \
+  > po/POTFILES
+ninja -C "${MESON_BUILD_ROOT}" iptux-pot
+ninja -C "${MESON_BUILD_ROOT}" iptux-update-po
+for f in po/*.po; do echo -n "$f: "; msgfmt -v $f; done
