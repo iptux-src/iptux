8ea6c1cbf1da19a86538daef0787938f5e8f67db
LI Daobing <lidaobing@gmail.com>
2024-06-07 22:50:20 -0700

    #598 add an option to compile with -fsanitize=address (#599)
    Currently, with `-fsanitize=address` compile flags, the make test will fail.

--- src/iptux-core/internal/UdpDataServiceTest.cpp
+++ src/iptux-core/internal/UdpDataServiceTest.cpp
@@ -1,5 +1,7 @@
 #include "gtest/gtest.h"
 
+#include <memory>
+
 #include "iptux-core/TestHelper.h"
 #include "iptux-core/internal/UdpDataService.h"
 #include "iptux-utils/utils.h"
@@ -9,13 +11,13 @@ using namespace iptux;
 
 TEST(UdpDataService, process) {
   auto core = newCoreThread();
-  auto service = new UdpDataService(*core.get());
+  auto service = make_unique<UdpDataService>(*core.get());
   service->process(inAddrFromString("127.0.0.1"), 1234, "", 0, true);
 }
 
 TEST(UdpDataService, SomeoneEntry) {
   auto core = newCoreThread();
-  auto service = new UdpDataService(*core.get());
+  auto service = make_unique<UdpDataService>(*core.get());
   const char* data = "iptux 0.8.0:1:lidaobing:lidaobing.lan:257:lidaobing";
   service->process(inAddrFromString("127.0.0.1"), 1234, data, strlen(data),
                    true);
@@ -28,7 +30,7 @@ TEST(UdpDataService, CreatePalInfo) {
         "1_iptux "
         "0.8.0-b1:6:lidaobing:LIs-MacBook-Pro.local:259:中\xe4\xb8\x00\x00icon-"
         "tux.png\x00utf-8\x00";
-    auto service = new UdpDataService(*core.get());
+    auto service = make_unique<UdpDataService>(*core.get());
     auto udp = service->process(inAddrFromString("127.0.0.1"), 1234, data,
                                 strlen(data), false);
     auto pal = udp->CreatePalInfo();
@@ -43,7 +45,7 @@ TEST(UdpDataService, CreatePalInfo) {
         "1_iptux "
         "0.8.0-b1:6:中\xe4\xb8:LIs-MacBook-Pro.local:259:"
         "中\xe4\xb8\x00\x00icon-tux.png\x00utf-8\x00";
-    auto service = new UdpDataService(*core.get());
+    auto service = make_unique<UdpDataService>(*core.get());
     auto udp = service->process(inAddrFromString("127.0.0.1"), 1234, data,
                                 strlen(data), false);
     auto pal = udp->CreatePalInfo();
