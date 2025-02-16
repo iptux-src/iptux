iptux-src/iptux

commit 906cb8379b778bdcae2ad19350d4c474a3f075bb

#594: Support save Image from chat history (#595)

src/iptux/DialogBase.cpp
@@ -14,6 +14,7 @@
 #include "config.h"
 #include "DialogBase.h"
 
+#include "UiModels.h"
 #include <glib/gi18n.h>
 #include <glog/logging.h>
 #include <sys/stat.h>
@@ -72,14 +73,12 @@ void DialogBase::ClearSublayerGeneral() {
  * 清空聊天历史记录.
  */
 void DialogBase::ClearHistoryTextView() {
-  GtkWidget* widget;
   GtkTextBuffer* buffer;
   GtkTextTagTable* table;
   GtkTextIter start, end;
   GSList *taglist, *tlist;
 
-  widget = GTK_WIDGET(g_datalist_get_data(&widset, "history-textview-widget"));
-  buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(widget));
+  buffer = gtk_text_view_get_buffer(chat_history_widget);
   table = gtk_text_buffer_get_tag_table(buffer);
 
   /* 清除用于局部标记的GtkTextTag */
@@ -105,17 +104,14 @@ void DialogBase::ClearHistoryTextView() {
  * 滚动聊天历史记录区.
  */
 void DialogBase::ScrollHistoryTextview() {
-  GtkWidget* widget;
   GtkTextBuffer* buffer;
   GtkTextIter end;
   GtkTextMark* mark;
 
-  widget = GTK_WIDGET(g_datalist_get_data(&widset, "history-textview-widget"));
-  buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(widget));
+  buffer = gtk_text_view_get_buffer(chat_history_widget);
   gtk_text_buffer_get_end_iter(buffer, &end);
   mark = gtk_text_buffer_create_mark(buffer, NULL, &end, FALSE);
-  gtk_text_view_scroll_to_mark(GTK_TEXT_VIEW(widget), mark, 0.0, TRUE, 0.0,
-                               0.0);
+  gtk_text_view_scroll_to_mark(chat_history_widget, mark, 0.0, TRUE, 0.0, 0.0);
   gtk_text_buffer_delete_mark(buffer, mark);
 }
 
@@ -287,7 +283,6 @@ GtkWidget* DialogBase::CreateInputArea() {
  */
 GtkWidget* DialogBase::CreateHistoryArea() {
   GtkWidget *frame, *sw;
-  GtkWidget* widget;
 
   frame = gtk_frame_new(_("Chat History"));
   gtk_frame_set_shadow_type(GTK_FRAME(frame), GTK_SHADOW_ETCHED_IN);
@@ -298,20 +293,24 @@ GtkWidget* DialogBase::CreateHistoryArea() {
                                       GTK_SHADOW_ETCHED_IN);
   gtk_container_add(GTK_CONTAINER(frame), sw);
 
-  widget = gtk_text_view_new_with_buffer(grpinf->buffer);
-  gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(widget), FALSE);
-  gtk_text_view_set_editable(GTK_TEXT_VIEW(widget), FALSE);
-  gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(widget), GTK_WRAP_WORD);
-  gtk_container_add(GTK_CONTAINER(sw), widget);
-  g_signal_connect(widget, "key-press-event",
+  chat_history_widget =
+      GTK_TEXT_VIEW(gtk_text_view_new_with_buffer(grpinf->buffer));
+  gtk_text_view_set_cursor_visible(chat_history_widget, FALSE);
+  gtk_text_view_set_editable(chat_history_widget, FALSE);
+  gtk_text_view_set_wrap_mode(chat_history_widget, GTK_WRAP_WORD);
+  gtk_container_add(GTK_CONTAINER(sw), GTK_WIDGET(chat_history_widget));
+  g_signal_connect(chat_history_widget, "key-press-event",
                    G_CALLBACK(textview_key_press_event), NULL);
-  g_signal_connect(widget, "event-after", G_CALLBACK(textview_event_after),
-                   NULL);
-  g_signal_connect(widget, "motion-notify-event",
+  g_signal_connect(chat_history_widget, "event-after",
+                   G_CALLBACK(textview_event_after), NULL);
+  g_signal_connect(chat_history_widget, "motion-notify-event",
                    G_CALLBACK(textview_motion_notify_event), NULL);
-  g_signal_connect(widget, "visibility-notify-event",
+  g_signal_connect(chat_history_widget, "visibility-notify-event",
                    G_CALLBACK(textview_visibility_notify_event), NULL);
-  g_datalist_set_data(&widset, "history-textview-widget", widget);
+  g_signal_connect_data(grpinf->buffer, "insert-child-anchor",
+                        G_CALLBACK(DialogBase::OnChatHistoryInsertChildAnchor),
+                        this, NULL,
+                        (GConnectFlags)(G_CONNECT_AFTER | G_CONNECT_SWAPPED));
 
   /* 滚动消息到最末位置 */
   ScrollHistoryTextview();
@@ -795,7 +794,7 @@ GtkTextBuffer* DialogBase::getInputBuffer() {
   return grpinf->getInputBuffer();
 }
 
-void DialogBase::OnPasteClipboard(DialogBase* self, GtkTextView* textview) {
+void DialogBase::OnPasteClipboard(DialogBase*, GtkTextView* textview) {
   GtkClipboard* clipboard;
   GtkTextBuffer* buffer;
   GtkTextIter iter;
@@ -817,6 +816,98 @@ void DialogBase::OnPasteClipboard(DialogBase* self, GtkTextView* textview) {
       g_object_unref(pixbuf);
     }
   }
+}
+
+gboolean DialogBase::OnImageButtonPress(DialogBase*,
+                                        GdkEventButton event,
+                                        GtkEventBox* event_box) {
+  if (event.type != GDK_BUTTON_PRESS || event.button != 3) {
+    return FALSE;
+  }
+
+  GtkWidget* image = gtk_bin_get_child(GTK_BIN(event_box));
+  if (!GTK_IS_IMAGE(image)) {
+    LOG_ERROR("image not found in event box.");
+    return FALSE;
+  }
+
+  GtkWidget* menu = gtk_menu_new();
+  GtkWidget* menu_item = gtk_menu_item_new_with_label(_("Save Image"));
+  gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item);
+  g_signal_connect_swapped(menu_item, "activate",
+                           G_CALLBACK(DialogBase::OnSaveImage), image);
+  gtk_widget_show_all(menu);
+
+  gtk_menu_popup_at_pointer(GTK_MENU(menu), (GdkEvent*)&event);
+  return TRUE;
+}
+
+void DialogBase::OnChatHistoryInsertChildAnchor(DialogBase* self,
+                                                const GtkTextIter*,
+                                                GtkTextChildAnchor* anchor,
+                                                GtkTextBuffer*) {
+  const char* path =
+      (const char*)g_object_get_data(G_OBJECT(anchor), kObjectKeyImagePath);
+  if (!path) {
+    LOG_ERROR("No image path found in anchor.");
+    return;
+  }
+
+  GtkWidget* event_box = gtk_event_box_new();
+  gtk_widget_set_focus_on_click(event_box, TRUE);
+
+  GtkImage* image = igtk_image_new_with_size(path, 300, 300);
+  if (!image) {
+    LOG_ERROR("Failed to create image widget.");
+    return;
+  }
+  g_object_set_data_full(G_OBJECT(image), kObjectKeyImagePath, g_strdup(path),
+                         g_free);
+
+  gtk_container_add(GTK_CONTAINER(event_box), GTK_WIDGET(image));
+  g_signal_connect_swapped(event_box, "button-press-event",
+                           G_CALLBACK(DialogBase::OnImageButtonPress), self);
+
+  gtk_text_view_add_child_at_anchor(self->chat_history_widget, event_box,
+                                    anchor);
+  gtk_widget_show_all(event_box);
+}
+
+void DialogBase::OnSaveImage(GtkImage* image) {
+  const char* path =
+      (const char*)g_object_get_data(G_OBJECT(image), kObjectKeyImagePath);
+  if (!path) {
+    LOG_ERROR("No image path found in image widget.");
+    return;
+  }
+
+  GtkWidget* dialog = gtk_file_chooser_dialog_new(
+      _("Save Image"), GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(image))),
+      GTK_FILE_CHOOSER_ACTION_SAVE, _("_Cancel"), GTK_RESPONSE_CANCEL,
+      _("_Save"), GTK_RESPONSE_ACCEPT, NULL);
+  gtk_file_chooser_set_do_overwrite_confirmation(GTK_FILE_CHOOSER(dialog),
+                                                 TRUE);
+  gtk_file_chooser_set_current_name(GTK_FILE_CHOOSER(dialog), "image.png");
+
+  if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
+    char* save_path = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+
+    GError* error = NULL;
+    GFile* source = g_file_new_for_path(path);
+    GFile* destination = g_file_new_for_path(save_path);
+    gboolean success = g_file_copy(source, destination, G_FILE_COPY_OVERWRITE,
+                                   NULL, NULL, NULL, &error);
+    if (!success) {
+      LOG_ERROR("Failed to save image: %s", error->message);
+      g_error_free(error);
+    }
+
+    g_object_unref(source);
+    g_object_unref(destination);
+    g_free(save_path);
+  }
+
+  gtk_widget_destroy(dialog);
 }
 
 }  // namespace iptux

src/iptux/DialogBase.h
@@ -82,11 +82,20 @@ class DialogBase : public SessionAbstract, public sigc::trackable {
   static gboolean UpdateFileSendUI(DialogBase* dlggrp);
   static void RemoveSelectedEnclosure(DialogBase* self);
   static void OnPasteClipboard(DialogBase* self, GtkTextView* textview);
+  static gboolean OnImageButtonPress(DialogBase* self,
+                                     GdkEventButton event,
+                                     GtkEventBox* eventbox);
+  static void OnChatHistoryInsertChildAnchor(DialogBase* self,
+                                             const GtkTextIter* location,
+                                             GtkTextChildAnchor* anchor,
+                                             GtkTextBuffer* buffer);
+  static void OnSaveImage(GtkImage* self);
 
  protected:
   Application* app;
   std::shared_ptr<ProgramData> progdt;
 
+  GtkTextView* chat_history_widget = 0;
   GtkTreeView* fileSendTree = 0;
   GtkTextView* inputTextviewWidget = 0;
 

src/iptux/UiHelper.cpp
@@ -363,4 +363,17 @@ string StrFirstNonEmptyLine(const string& s) {
   return s.substr(pos, pos2 - pos);
 }
 
+GtkImage* igtk_image_new_with_size(const char* filename,
+                                   int width,
+                                   int height) {
+  GdkPixbuf* pixbuf = gdk_pixbuf_new_from_file(filename, NULL);
+  if (!pixbuf) {
+    LOG_ERROR("Error loading image.");
+    return NULL;
+  }
+
+  pixbuf_shrink_scale_1(&pixbuf, width, height);
+  return GTK_IMAGE(gtk_image_new_from_pixbuf(pixbuf));
+}
+
 }  // namespace iptux

src/iptux/UiModels.cpp
@@ -17,6 +17,8 @@ using namespace std;
 
 namespace iptux {
 
+const char* const kObjectKeyImagePath = "image-path";
+
 /**
  * 文件传输树(trans-tree)底层数据结构.
  * 14,0 status,1 task,2 peer,3 ip,4 filename,5 filelength,6 finishlength,7
@@ -622,6 +624,8 @@ static void InsertStringToBuffer(GtkTextBuffer* buffer, const gchar* s) {
   }
   g_match_info_free(matchinfo);
   gtk_text_buffer_insert(buffer, &iter, string + urlendp, -1);
+  gtk_text_buffer_get_end_iter(buffer, &iter);
+  gtk_text_buffer_insert(buffer, &iter, "\n", -1);
 }
 
 /**
@@ -631,7 +635,8 @@ static void InsertStringToBuffer(GtkTextBuffer* buffer, const gchar* s) {
  */
 static void InsertHeaderToBuffer(GtkTextBuffer* buffer,
                                  const MsgPara* para,
-                                 CPPalInfo me) {
+                                 CPPalInfo me,
+                                 time_t now) {
   GtkTextIter iter;
   gchar* header;
 
@@ -640,21 +645,22 @@ static void InsertHeaderToBuffer(GtkTextBuffer* buffer,
    */
   switch (para->stype) {
     case MessageSourceType::PAL:
-      header = getformattime(FALSE, "%s", para->getPal()->getName().c_str());
+      header =
+          getformattime2(now, FALSE, "%s", para->getPal()->getName().c_str());
       gtk_text_buffer_get_end_iter(buffer, &iter);
       gtk_text_buffer_insert_with_tags_by_name(buffer, &iter, header, -1,
                                                "pal-color", NULL);
       g_free(header);
       break;
     case MessageSourceType::SELF:
-      header = getformattime(FALSE, "%s", me->getName().c_str());
+      header = getformattime2(now, FALSE, "%s", me->getName().c_str());
       gtk_text_buffer_get_end_iter(buffer, &iter);
       gtk_text_buffer_insert_with_tags_by_name(buffer, &iter, header, -1,
                                                "me-color", NULL);
       g_free(header);
       break;
     case MessageSourceType::ERROR:
-      header = getformattime(FALSE, "%s", _("<ERROR>"));
+      header = getformattime2(now, FALSE, "%s", _("<ERROR>"));
       gtk_text_buffer_get_end_iter(buffer, &iter);
       gtk_text_buffer_insert_with_tags_by_name(buffer, &iter, header, -1,
                                                "error-color", NULL);
@@ -663,36 +669,33 @@ static void InsertHeaderToBuffer(GtkTextBuffer* buffer,
     default:
       break;
   }
+  gtk_text_buffer_get_end_iter(buffer, &iter);
+  gtk_text_buffer_insert(buffer, &iter, "\n", -1);
 }
 
-#define OCCUPY_OBJECT 0x01
-
 /**
- * 插入图片到TextBuffer(非UI线程安全).
+ * 插入图片到TextBuffer.
  * @param buffer text-buffer
  * @param path 图片路径
  */
 static void InsertPixbufToBuffer(GtkTextBuffer* buffer, const gchar* path) {
-  GtkTextIter start, end;
-  GdkPixbuf* pixbuf;
+  GtkTextIter iter;
 
-  if ((pixbuf = gdk_pixbuf_new_from_file(path, NULL))) {
-    gtk_text_buffer_get_start_iter(buffer, &start);
-    if (gtk_text_iter_get_char(&start) == OCCUPY_OBJECT ||
-        gtk_text_iter_forward_find_char(
-            &start, GtkTextCharPredicate(giter_compare_foreach),
-            GUINT_TO_POINTER(OCCUPY_OBJECT), NULL)) {
-      end = start;
-      gtk_text_iter_forward_char(&end);
-      gtk_text_buffer_delete(buffer, &start, &end);
-    }
-    gtk_text_buffer_insert_pixbuf(buffer, &start, pixbuf);
-    g_object_unref(pixbuf);
-  }
+  gtk_text_buffer_get_end_iter(buffer, &iter);
+  GtkTextChildAnchor* anchor = gtk_text_child_anchor_new();
+  g_object_set_data_full(G_OBJECT(anchor), kObjectKeyImagePath, g_strdup(path),
+                         GDestroyNotify(g_free));
+  gtk_text_buffer_insert_child_anchor(buffer, &iter, anchor);
+  gtk_text_buffer_get_end_iter(buffer, &iter);
+  gtk_text_buffer_insert(buffer, &iter, "\n", -1);
 }
 
 void GroupInfo::addMsgPara(const MsgPara& para) {
-  GtkTextIter iter;
+  time_t now = time(NULL);
+  _addMsgPara(para, now);
+}
+
+void GroupInfo::_addMsgPara(const MsgPara& para, time_t now) {
   const gchar* data;
 
   time(&last_activity_);
@@ -702,18 +705,15 @@ void GroupInfo::addMsgPara(const MsgPara& para) {
     data = chipData->data.c_str();
     switch (chipData->type) {
       case MESSAGE_CONTENT_TYPE_STRING:
-        InsertHeaderToBuffer(buffer, &para, me);
-        gtk_text_buffer_get_end_iter(buffer, &iter);
-        gtk_text_buffer_insert(buffer, &iter, "\n", -1);
+        InsertHeaderToBuffer(buffer, &para, me, now);
         InsertStringToBuffer(buffer, data);
-        gtk_text_buffer_get_end_iter(buffer, &iter);
-        gtk_text_buffer_insert(buffer, &iter, "\n", -1);
         last_message_ = StrFirstNonEmptyLine(chipData->data);
         if (logSystem) {
           logSystem->communicateLog(&para, "[STRING]%s", data);
         }
         break;
       case MESSAGE_CONTENT_TYPE_PICTURE:
+        InsertHeaderToBuffer(buffer, &para, me, now);
         InsertPixbufToBuffer(buffer, data);
         last_message_ = _("[IMG]");
         if (logSystem) {

src/iptux/UiModels.h
@@ -10,6 +10,8 @@
 
 namespace iptux {
 
+extern const char* const kObjectKeyImagePath;
+
 typedef void (*GActionCallback)(GSimpleAction* action,
                                 GVariant* parameter,
                                 gpointer user_data);
@@ -81,6 +83,7 @@ class GroupInfo {
   bool hasPal(PPalInfo pal) const;
 
   void addMsgPara(const MsgPara& msg);
+  void _addMsgPara(const MsgPara& msg, time_t t);
   void readAllMsg();
   int getUnreadMsgCount() const;
   std::string GetInfoAsMarkup(GroupInfoStyle style) const;

src/iptux/DialogPeerTest.cpp
@@ -21,6 +21,7 @@ TEST(DialogPeer, Constructor) {
   app->getCoreThread()->AttachPalToList(pal);
 
   GroupInfo* grpinf = app->getCoreThread()->GetPalRegularItem(pal.get());
+  grpinf->buffer = gtk_text_buffer_new(NULL);
   DialogPeer* dlgpr = new DialogPeer(app, grpinf);
 
   auto clipboard = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD);
@@ -37,6 +38,16 @@ TEST(DialogPeer, Constructor) {
   gtk_clipboard_set_image(clipboard, pixbuf);
   do_action(dlgpr, "paste");
   g_object_unref(pixbuf);
+
+  MsgPara msg(pal);
+  msg.dtlist.push_back(ChipData("helloworld"));
+
+  grpinf->addMsgPara(msg);
+
+  msg = MsgPara(pal);
+  msg.dtlist.push_back(
+      ChipData(MessageContentType::PICTURE, testDataPath("iptux.png")));
+  grpinf->addMsgPara(msg);
 
   DestroyApplication(app);
 }

src/iptux/UiModelsTest.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 
+#include "iptux-utils/TestHelper.h"
 #include <memory>
 #include <vector>
 
@@ -75,4 +76,44 @@ TEST(GroupInfo, GetHintAsMarkup) {
             "127.0.0.1\nCompatibility: Microsoft\nSystem coding: "
             "\nSignature:\n<span foreground=\"#00FF00\" font_style=\"italic\" "
             "size=\"smaller\">hello</span>");
+}
+
+static string igtk_text_get_all_text(GtkTextBuffer* buffer) {
+  GtkTextIter start, end;
+  gtk_text_buffer_get_start_iter(buffer, &start);
+  gtk_text_buffer_get_end_iter(buffer, &end);
+  char* s = gtk_text_buffer_get_slice(buffer, &start, &end, true);
+  string ret(s);
+  g_free(s);
+  return ret;
+}
+
+TEST(GroupInfo, addMsgPara) {
+  PalInfo pal("127.0.0.1", 2425);
+  pal.setVersion("1_iptux");
+  pal.setName("palname");
+  PalInfo me("127.0.0.2", 2425);
+  PPalInfo cpal = make_shared<PalInfo>(pal);
+  CPPalInfo cme = make_shared<PalInfo>(me);
+  GroupInfo gi(cpal, cme, nullptr);
+  gi.buffer = gtk_text_buffer_new(nullptr);
+
+  MsgPara msg(cpal);
+  msg.dtlist.push_back(ChipData("helloworld"));
+
+  time_t now = 1716533706;
+  setenv("TZ", "GMT", 1);
+  tzset();
+
+  gi._addMsgPara(msg, now);
+  ASSERT_EQ(igtk_text_get_all_text(gi.buffer),
+            "(06:55:06) palname:\nhelloworld\n");
+
+  msg = MsgPara(cpal);
+  msg.dtlist.push_back(
+      ChipData(MessageContentType::PICTURE, testDataPath("iptux.png")));
+  gi._addMsgPara(msg, now + 1);
+  ASSERT_EQ(
+      igtk_text_get_all_text(gi.buffer),
+      "(06:55:06) palname:\nhelloworld\n(06:55:07) palname:\n\xEF\xBF\xBC\n");
 }

commit 9adbf693dcf0e6f421195eb7212cf7d414b1f9c7

#658: after close dialog and reopen, the image no longer exist (#659)
1. restore the images from text-buffer after dialog reopened. (bug introduced in #595)
2. when dialog closed, it SHOULD remove the event listener on history text buffer (bug introduced in #595)
3. keep the sent-images(otherwise no way to restore the image), and keep it under the `~/.cache/iptux/sent-image/`

src/api/iptux-core/Models.h
@@ -206,12 +206,6 @@ class ChipData {
 
   MessageContentType type;  ///< 消息内容类型
   std::string data;         ///< 数据串 *
-
-  void SetDeleteFileAfterSent(bool val) { deleteFileAfterSent = val; }
-  bool GetDeleteFileAfterSent() const { return deleteFileAfterSent; }
-
- private:
-  bool deleteFileAfterSent{true};
 };
 
 /**

src/config.h.in
@@ -17,6 +17,7 @@
 #define PIC_PATH "/iptux/pic"
 #define ICON_PATH "/iptux/icon"
 #define PHOTO_PATH "/iptux/photo"
+#define SENT_IMAGE_PATH "/iptux/sent-image"
 
 #mesondefine SYSTEM_DARWIN
 #mesondefine HAVE_APPINDICATOR

src/iptux-core/CoreThread.cpp
@@ -1,6 +1,8 @@
 #include "config.h"
 #include "iptux-core/CoreThread.h"
 
+#include "Const.h"
+#include <cstdio>
 #include <deque>
 #include <fstream>
 #include <functional>
@@ -44,22 +46,25 @@ void init_iptux_environment() {
 
   env = g_get_user_cache_dir();
   if (access(env, F_OK) != 0)
-    g_mkdir(env, 0777);
+    g_mkdir(env, 0755);
   snprintf(path, MAX_PATHLEN, "%s" IPTUX_PATH, env);
   if (access(path, F_OK) != 0)
-    g_mkdir(path, 0777);
+    g_mkdir(path, 0755);
   snprintf(path, MAX_PATHLEN, "%s" PIC_PATH, env);
   if (access(path, F_OK) != 0)
-    g_mkdir(path, 0777);
+    g_mkdir(path, 0755);
   snprintf(path, MAX_PATHLEN, "%s" PHOTO_PATH, env);
   if (access(path, F_OK) != 0)
-    g_mkdir(path, 0777);
+    g_mkdir(path, 0755);
   snprintf(path, MAX_PATHLEN, "%s" ICON_PATH, env);
   if (access(path, F_OK) != 0)
-    g_mkdir(path, 0777);
+    g_mkdir(path, 0755);
   snprintf(path, MAX_PATHLEN, "%s" LOG_PATH, env);
   if (access(path, F_OK) != 0)
-    g_mkdir(path, 0777);
+    g_mkdir(path, 0755);
+  snprintf(path, MAX_PATHLEN, "%s" SENT_IMAGE_PATH, env);
+  if (access(path, F_OK) != 0)
+    g_mkdir(path, 0755);
 
   env = g_get_user_config_dir();
   if (access(env, F_OK) != 0)
@@ -493,10 +498,6 @@ bool CoreThread::SendMessage(CPPalInfo pal, const ChipData& chipData) {
       }
       Command(*this).SendSublayer(sock, pal, IPTUX_MSGPICOPT, ptr);
       close(sock);  // 关闭网络套接口
-      /*/* 删除此图片 */
-      if (chipData.GetDeleteFileAfterSent()) {
-        unlink(ptr);  // 此文件已无用处
-      }
       return true;
     default:
       g_assert_not_reached();

src/iptux/DialogBase.cpp
@@ -45,6 +45,8 @@ DialogBase::~DialogBase() {
   if (timersend > 0)
     g_source_remove(timersend);
   ClearSublayerGeneral();
+  if (grpinf)
+    grpinf->clearDialog();
 }
 
 /**
@@ -63,7 +65,6 @@ void DialogBase::ClearSublayerGeneral() {
   if (progdt->IsAutoCleanChatHistory()) {
     ClearHistoryTextView();
   }
-  grpinf->clearDialog();
   g_datalist_clear(&widset);
   g_datalist_clear(&mdlset);
   g_datalist_clear(&dtset);
@@ -277,6 +278,23 @@ GtkWidget* DialogBase::CreateInputArea() {
   return frame;
 }
 
+static void iptux_dlg_refresh_anchors(DialogBase* dialog,
+                                      GtkTextBuffer* buffer) {
+  GtkTextIter start, end;
+  gtk_text_buffer_get_bounds(buffer, &start, &end);
+
+  GtkTextIter iter = start;
+  while (!gtk_text_iter_equal(&iter, &end)) {
+    GtkTextChildAnchor* anchor = gtk_text_iter_get_child_anchor(&iter);
+
+    if (anchor) {
+      dialog->onChatHistoryInsertChildAnchor(anchor);
+    }
+
+    gtk_text_iter_forward_char(&iter);
+  }
+}
+
 /**
  * 创建聊天历史记录区域.
  * @return 主窗体.
@@ -305,11 +323,9 @@ GtkWidget* DialogBase::CreateHistoryArea() {
                    G_CALLBACK(textview_event_after), NULL);
   g_signal_connect(chat_history_widget, "motion-notify-event",
                    G_CALLBACK(textview_motion_notify_event), NULL);
-  g_signal_connect_data(grpinf->buffer, "insert-child-anchor",
-                        G_CALLBACK(DialogBase::OnChatHistoryInsertChildAnchor),
-                        this, NULL,
-                        (GConnectFlags)(G_CONNECT_AFTER | G_CONNECT_SWAPPED));
-
+  if (grpinf->buffer) {
+    iptux_dlg_refresh_anchors(this, grpinf->buffer);
+  }
   /* 滚动消息到最末位置 */
   ScrollHistoryTextview();
 
@@ -847,10 +863,7 @@ gboolean DialogBase::OnImageButtonPress(DialogBase* self,
   return TRUE;
 }
 
-void DialogBase::OnChatHistoryInsertChildAnchor(DialogBase* self,
-                                                const GtkTextIter*,
-                                                GtkTextChildAnchor* anchor,
-                                                GtkTextBuffer*) {
+void DialogBase::onChatHistoryInsertChildAnchor(GtkTextChildAnchor* anchor) {
   const char* path =
       (const char*)g_object_get_data(G_OBJECT(anchor), kObjectKeyImagePath);
   if (!path) {
@@ -871,9 +884,9 @@ void DialogBase::OnChatHistoryInsertChildAnchor(DialogBase* self,
 
   gtk_container_add(GTK_CONTAINER(event_box), GTK_WIDGET(image));
   g_signal_connect_swapped(event_box, "button-press-event",
-                           G_CALLBACK(DialogBase::OnImageButtonPress), self);
+                           G_CALLBACK(DialogBase::OnImageButtonPress), this);
 
-  gtk_text_view_add_child_at_anchor(self->chat_history_widget, event_box,
+  gtk_text_view_add_child_at_anchor(this->chat_history_widget, event_box,
                                     anchor);
   gtk_widget_show_all(event_box);
 }

src/iptux/DialogBase.h
@@ -30,6 +30,7 @@ class DialogBase : public SessionAbstract, public sigc::trackable {
 
   virtual GtkWindow* getWindow() = 0;
   void ClearHistoryTextView();
+  void onChatHistoryInsertChildAnchor(GtkTextChildAnchor* anchor);
 
  protected:
   void InitSublayerGeneral();
@@ -87,10 +88,6 @@ class DialogBase : public SessionAbstract, public sigc::trackable {
   static gboolean OnImageButtonPress(DialogBase* self,
                                      GdkEventButton* event,
                                      GtkEventBox* eventbox);
-  static void OnChatHistoryInsertChildAnchor(DialogBase* self,
-                                             const GtkTextIter* location,
-                                             GtkTextChildAnchor* anchor,
-                                             GtkTextBuffer* buffer);
   static void OnSaveImage(DialogBase* self);
   static void OnCopyImage(DialogBase* self);
 

src/iptux/UiCoreThread.cpp
@@ -355,7 +355,7 @@ GroupInfo* UiCoreThread::AttachPalRegularItem(PPalInfo pal) {
 
   grpinf = new GroupInfo(pal, getMe(), logSystem);
   grpinf->grpid = inAddrToUint32(pal->ipv4());
-  grpinf->buffer = gtk_text_buffer_new(tag_table_);
+  grpinf->initBuffer(tag_table_);
   grpinf->clearDialog();
   grpinf->signalUnreadMsgCountUpdated.connect(
       sigc::mem_fun(*this, &UiCoreThread::onGroupInfoMsgCountUpdate));
@@ -380,7 +380,7 @@ GroupInfo* UiCoreThread::AttachPalSegmentItem(PPalInfo pal) {
   grpinf = new GroupInfo(GROUP_BELONG_TYPE_SEGMENT, vector<PPalInfo>(), getMe(),
                          name, logSystem);
   grpinf->grpid = g_quark_from_static_string(name.c_str());
-  grpinf->buffer = gtk_text_buffer_new(tag_table_);
+  grpinf->initBuffer(tag_table_);
   grpinf->clearDialog();
   sgmlist = g_slist_append(sgmlist, grpinf);
 
@@ -400,7 +400,7 @@ GroupInfo* UiCoreThread::AttachPalGroupItem(PPalInfo pal) {
   }
   grpinf = new GroupInfo(GROUP_BELONG_TYPE_GROUP, vector<PPalInfo>(), getMe(),
                          name, logSystem);
-  grpinf->buffer = gtk_text_buffer_new(tag_table_);
+  grpinf->initBuffer(tag_table_);
   grpinf->clearDialog();
   grplist = g_slist_append(grplist, grpinf);
 
@@ -421,7 +421,7 @@ GroupInfo* UiCoreThread::AttachPalBroadcastItem(PPalInfo) {
   grpinf = new GroupInfo(GROUP_BELONG_TYPE_BROADCAST, vector<PPalInfo>(),
                          getMe(), name, logSystem);
   grpinf->grpid = g_quark_from_static_string(name);
-  grpinf->buffer = gtk_text_buffer_new(tag_table_);
+  grpinf->initBuffer(tag_table_);
   grpinf->clearDialog();
   brdlist = g_slist_append(brdlist, grpinf);
 

src/iptux/UiHelper.cpp
@@ -380,9 +380,13 @@ string StrFirstNonEmptyLine(const string& s) {
 GtkImage* igtk_image_new_with_size(const char* filename,
                                    int width,
                                    int height) {
-  GdkPixbuf* pixbuf = gdk_pixbuf_new_from_file(filename, NULL);
+  GError* error = NULL;
+
+  GdkPixbuf* pixbuf = gdk_pixbuf_new_from_file(filename, &error);
   if (!pixbuf) {
-    LOG_ERROR("Error loading image.");
+    LOG_ERROR("Error loading image '%s': [%d]%s", filename, error->code,
+              error->message);
+    g_error_free(error);
     return NULL;
   }
 

src/iptux/UiModels.cpp
@@ -473,7 +473,7 @@ bool GroupInfo::isInputEmpty() const {
 }
 
 shared_ptr<MsgPara> GroupInfo::genMsgParaFromInput() const {
-  static uint32_t count = 0;
+  static unsigned int count = 0;
   GtkTextIter start;
   GdkPixbuf* pixbuf;
   char buf[7];
@@ -493,8 +493,9 @@ shared_ptr<MsgPara> GroupInfo::genMsgParaFromInput() const {
         oss.str("");
       }
       pixbuf = gtk_text_iter_get_pixbuf(&start);
-      chipmsg = g_strdup_printf("%s" IPTUX_PATH "/%" PRIx32,
-                                g_get_user_config_dir(), count++);
+      chipmsg =
+          g_strdup_printf("%s" SENT_IMAGE_PATH "/%d.png",
+                          g_get_user_cache_dir(), g_atomic_int_add(&count, 1));
       GError* error = nullptr;
       gdk_pixbuf_save(pixbuf, chipmsg, "png", &error, NULL);
       if (error) {
@@ -646,6 +647,24 @@ string GroupInfo::GetHintAsMarkup() const {
   }
 
   return res.str();
+}
+
+void GroupInfo::initBuffer(GtkTextTagTable* tag_table) {
+  this->buffer = gtk_text_buffer_new(tag_table);
+  g_signal_connect_data(this->buffer, "insert-child-anchor",
+                        G_CALLBACK(GroupInfo::OnBufferInsertChildAnchor), this,
+                        NULL,
+                        (GConnectFlags)(G_CONNECT_AFTER | G_CONNECT_SWAPPED));
+}
+
+void GroupInfo::OnBufferInsertChildAnchor(GroupInfo* self,
+                                          const GtkTextIter*,
+                                          GtkTextChildAnchor* anchor,
+                                          GtkTextBuffer*) {
+  if (!self->dialogBase)
+    return;
+
+  self->dialogBase->onChatHistoryInsertChildAnchor(anchor);
 }
 
 /**

src/iptux/UiModels.h
@@ -105,9 +105,17 @@ class GroupInfo {
   time_t last_activity() const { return last_activity_; }
   const std::string& last_message() const { return last_message_; }
 
+  void initBuffer(GtkTextTagTable* tag_table);
+
  public:
   sigc::signal<void(GroupInfo*, int, int)> signalUnreadMsgCountUpdated;
   sigc::signal<void(GroupInfo*)> signalNewFileReceived;
+
+ private:
+  static void OnBufferInsertChildAnchor(GroupInfo* self,
+                                        const GtkTextIter* location,
+                                        GtkTextChildAnchor* anchor,
+                                        GtkTextBuffer* buffer);
 
  public:
   GQuark grpid;           ///< 唯一标识

src/iptux-core/CoreThreadTest.cpp
@@ -205,7 +205,6 @@ TEST(CoreThread, FullCase) {
 
   // send picture
   ChipData chipData(MessageContentType::PICTURE, testDataPath("iptux.png"));
-  chipData.SetDeleteFileAfterSent(false);
   thread1->SendMessage(pal2InThread1, chipData);
   // WARNING: does not work as expected, the message will be sent from
   // 127.0.0.2(expect 127.0.0.1) while(thread2Events.size() != 2) {

src/iptux/DialogPeerTest.cpp
@@ -22,7 +22,7 @@ TEST(DialogPeer, Constructor) {
   app->getCoreThread()->AttachPalToList(pal);
 
   GroupInfo* grpinf = app->getCoreThread()->GetPalRegularItem(pal.get());
-  grpinf->buffer = gtk_text_buffer_new(NULL);
+  grpinf->initBuffer(NULL);
   DialogPeer* dlgpr = new DialogPeer(app, grpinf);
   ASSERT_EQ(igtk_text_buffer_get_text(grpinf->getInputBuffer()), "");
 

src/iptux/UiModelsTest.cpp
@@ -98,7 +98,7 @@ TEST(GroupInfo, addMsgPara) {
   PPalInfo cpal = make_shared<PalInfo>(pal);
   CPPalInfo cme = make_shared<PalInfo>(me);
   GroupInfo gi(cpal, cme, nullptr);
-  gi.buffer = gtk_text_buffer_new(nullptr);
+  gi.initBuffer(NULL);
 
   MsgPara msg(cpal);
   msg.dtlist.push_back(ChipData("helloworld"));