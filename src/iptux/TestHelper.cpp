6a757a4f24a9404cd6f7408a03e6404f4e01bb89
LI Daobing <lidaobing@gmail.com>
2025-07-07 23:18:54 -0700

    1

--- src/iptux/TestHelper.cpp
+++ src/iptux/TestHelper.cpp
@@ -11,6 +11,7 @@ Application* CreateApplication() {
   gtk_init(nullptr, nullptr);
   auto config = newTestIptuxConfig();
   Application* app = new Application(config);
+  app->set_enable_app_indicator(false);
   app->startup();
   // g_application_register(G_APPLICATION(app->getApp()), nullptr, nullptr);
   // auto i = g_application_get_is_registered(G_APPLICATION(app->getApp()));
