08dab7484f371067475d207852360586e6d5515f
LI Daobing <lidaobing@gmail.com>
2026-07-26 21:34:57 -0700

    1

--- src/iptux-gi/gi-test.py
+++ src/iptux-gi/gi-test.py
@@ -1,5 +1,5 @@
 import gi
-gi.require_version('Iptux', '1.0')
+gi.require_version('Iptux', '0.10')
 gi.require_version('GLib', '2.0')
 from gi.repository import Iptux
 from gi.repository import GLib
